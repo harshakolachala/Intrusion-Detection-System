96258cbafbad473dfde8282df3841725720aaeb7
Sairohith-K <sairohith1011@gmail.com>
2026-08-05 14:32:52 +0530

    main.tsx updated

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeModeProvider } from "./context/ThemeModeContext";
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeModeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeModeProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
